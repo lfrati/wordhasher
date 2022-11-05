e373cdf0a7e321c63774c6a7013ccdf3103694c3
lfrati <fratilapo@gmail.com>
2022-11-05 17:55:33 -0400

    Remove local folder. Streamline code.

--- test_wordhasher.py
+++ test_wordhasher.py
@@ -1,26 +1,45 @@
-from wordhasher import WordHasher
+import unittest
 from tqdm import trange
+from wordhasher import *
 
-whash = WordHasher()
-print()
-whash.uninstall()
-whash.install()
+from itertools import chain, combinations
 
-test_string = "This is a test!"
-print(f"\n Test string: {test_string}\nWordnet-hash: {whash.encode(test_string)}\n")
 
-file = __file__
-print(f"Self encoding: {file}\n Wordnet-hash: {whash.encode_file(file)}\n")
+def subsets(s):
+    opts = chain.from_iterable(combinations(s, r) for r in range(1, len(s) + 1))
+    yield from ["".join(opt) for opt in opts]
 
-print("Checking for collisions")
-N = 10_000_000
-seen = set()
-for i in trange(N):
-    sample = whash.sample()
-    if sample not in seen:
-        seen.add(sample)
-    else:
-        print(f"Collision after {i} samples. Unlucky :(")
-        break
-else:
-    print(f"{N - len(seen)} collisions in {N:_} samples.\n")
+
+class WhasherTests(unittest.TestCase):
+    def test_wrong_mode(self):
+        wh = WordHasher()
+        with self.assertRaises(AssertionError):
+            wh.sample(mode="nvaNk")
+
+    def test_collisions(self):
+        wh = WordHasher()
+        N = 1_000_000
+        seen = set()
+        for i in trange(N):
+            sample = wh.sample()
+            if sample not in seen:
+                seen.add(sample)
+            else:
+                print(f"Collision after {i} samples. Unlucky :(")
+                break
+        self.assertEqual(len(seen), N)
+
+    def test_sampling(self):
+        wh = WordHasher()
+        modes = subsets(wh.modes)
+        for mode in modes:
+            try:
+                wh.sample(mode=mode)
+            except AssertionError:
+                self.fail(f"Invalid sampling mode : {mode}")
+        with self.assertRaises(AssertionError):
+            wh.sample(mode="")
+
+
+if __name__ == "__main__":
+    unittest.main()
--- wordhasher.py
+++ wordhasher.py
@@ -1,90 +1,58 @@
 import hashlib
-from pathlib import Path
 import random
-import shutil
-
-import requests
 
 
 class WordHasher:
     def __init__(self):
+        with open("nouns.txt", "r") as f:
+            self.nouns = f.read().splitlines()
+        with open("adjectives.txt", "r") as f:
+            self.adjectives = f.read().splitlines()
+        with open("verbs.txt", "r") as f:
+            self.verbs = f.read().splitlines()
 
-        self.storage = Path().home() / ".wordhasher"
-        self.filenames = [
-            self.storage / name for name in ["nouns.txt", "adjectives.txt", "verbs.txt"]
-        ]
+        self.samplers = {
+            "v": lambda: self.verbs[random.randrange(len(self.verbs))],
+            "a": lambda: self.adjectives[random.randrange(len(self.adjectives))],
+            "n": lambda: self.nouns[random.randrange(len(self.nouns))],
+            "N": lambda: str(random.randrange(1000)),
+        }
+        self.modes = set(list(self.samplers.keys()))
 
-        if not self.storage.exists():
-            self.install()
-
-        # to make the linter shut up
-        self.nouns = []
-        self.verbs = []
-        self.adjectives = []
-        for file in self.filenames:
-            name = file.stem
-            with open(file, "r") as f:
-                words = f.read().splitlines()
-                setattr(self, name, words)
-
-    def install(self):
-
-        print("Storing data in ", self.storage)
-        if not self.storage.exists():
-            self.storage.mkdir(parents=True)
-
-        for path in self.filenames:
-            filename = path.name
-
-            if not path.exists():
-                print(f"Downloading {path}...")
-                data = self.from_github(filename)
-                with open(path, "w") as f:
-                    f.write(data)
-            print(f"{filename} ready.")
-        print()
-
-    def from_github(self, filename: str) -> str:
-        data = requests.get(
-            f"https://raw.githubusercontent.com/lfrati/wordnetter/main/{filename}"
-        )
-        if data.ok:
-            return data.text
-        raise RuntimeError
-
-    def uninstall(self):
-        print("Deleting", self.storage)
-        shutil.rmtree(self.storage)
-        print("Removed.\n")
-
-    def encode(self, text):
+    def from_str(self, text):
         h = hashlib.sha1(text.encode("utf-8")).hexdigest()
-        n = int(h[:20], 16) % len(self.nouns)
-        v = int(h[20:30], 16) % len(self.verbs)
-        a = int(h[30], 16) % len(self.adjectives)
-        return self.get(n, v, a)
 
-    def encode_file(self, file):
+        n = int(h[:20], 16) % len(self.nouns)
+        noun = self.nouns[n]
+
+        a = int(h[30], 16) % len(self.adjectives)
+        adj = self.adjectives[a]
+
+        v = int(h[20:30], 16) % len(self.verbs)
+        verb = self.verbs[v]
+
+        wh = f"{verb}-{adj}-{noun}"
+        return wh
+
+    def from_file(self, file):
         with open(file, "r") as f:
             text = f.read()
-        return self.encode(text)
+        return self.from_str(text)
 
-    def get(self, n, v, a):
-        noun = self.nouns[n]
-        adj = self.adjectives[a]
-        verb = self.verbs[v]
-        words = f"{verb}-{adj}-{noun}"
-        return words
-
-    def sample(self):
-
-        n = random.randrange(len(self.nouns))
-        v = random.randrange(len(self.verbs))
-        a = random.randrange(len(self.adjectives))
-
-        return self.get(n, v, a) + f"-{random.randint(1,999)}"
+    def sample(self, mode: str = "vanN") -> str:
+        assert mode != ""
+        assert all(m in self.modes for m in mode)
+        parts = [self.samplers[m]() for m in mode]
+        return "-".join(parts)
 
 
 if __name__ == "__main__":
-    wnetter = Wordnetter()
-    print(wnetter.sample())
+    whash = WordHasher()
+    m = "This is a test."
+    print(f"Hashing {m} = {whash.from_str(m)}")
+    print(f"Hashing {m} = {whash.from_str(m)}")
+    print(f"Hashing {__file__} = {whash.from_str(__file__)}")
+    print(f"Hashing {__file__} = {whash.from_str(__file__)}")
+    print("Default sample:", whash.sample())
+    for mode in ["va", "an", "anN", "van", "vanN"]:
+        print(f"  mode:{mode:<5}: {whash.sample(mode=mode)}")
